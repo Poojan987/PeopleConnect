149c6f3766781f898de014e49c4eeae76f40a0e2
Poojan Gandhi <poojanngandhi@gmail.com>
2021-08-09 13:24:40 +0530

    deleted

--- Django/notifications/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import NotificationCountAPIView
-
-urlpatterns = [
-    path('count',NotificationCountAPIView.as_view()),
-]
--- frontend/src/components/notification.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function Notification(){
-
-    return(
-        <h1  >NOTIFICATIONS</h1>
-    )
-}
-
